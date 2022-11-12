1627c532268a2fe4d6a3d6dd1fe4de44eb95672e
bashir <bashirabdi1994@gmail.com>
2022-11-13 00:08:19 +0300

    adding testng priority for home test page and Dashboard test page

--- src/test/java/com/alisonTest/Dashboard/Dashboard.java
+++ src/test/java/com/alisonTest/Dashboard/Dashboard.java
@@ -3,12 +3,14 @@ package com.alisonTest.Dashboard;
 import com.alison.pages.Dashboard.DashboardPage;
 import com.alison.pages.Login.LoginPage;
 import com.alisonTest.Base.BrowserBase;
+import org.testng.annotations.Test;
 
 public class Dashboard  extends BrowserBase {
     DashboardPage dashboardPage =new DashboardPage();
     LoginPage loginPage = new LoginPage();
 
 
+    @Test(priority = 0)
     public void ClickLoginBtn(){
         dashboardPage = loginPage.ClickLogIn(driver);
     }
--- src/test/java/com/alisonTest/HomeTest/Home.java
+++ src/test/java/com/alisonTest/HomeTest/Home.java
@@ -4,26 +4,28 @@ import com.alison.pages.Home.HomePage;
 import com.alison.pages.Login.LoginPage;
 import com.alisonTest.Base.BrowserBase;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class Home extends BrowserBase {
     HomePage homePage = new HomePage();
     LoginPage loginPage =new LoginPage();
 
-
+@Test(priority = 1)
     public void LoginBtn(){
         loginPage =homePage.ClickLoginBtn(driver);
     }
-
+@Test(priority = 2)
     public void verifyTitle(){
         String expected = "Welcome Back!";
         String actual = loginPage.verifyTitle(driver);
         Assert.assertEquals(actual,expected);
 
     }
-
+@Test(priority = 3)
     public void  ClickEmail(){
         loginPage.ClickEmail(driver,"abdirahman.bashir88gmail.com");
     }
+    @Test(priority = 4)
     public void ClickPassword(){
         loginPage.EnterPassword(driver,"Abdi12345");
     }
